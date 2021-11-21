819c219352bb661407281c33a36df17d5c8735c1
yazzan-96 <yazanaraddadi@gmail.com>
2021-11-21 23:08:30 +0300

    add 2 table and 2 cell

---
+++ dawaay/ShowOrderCell.swift
@@ -0,0 +1,23 @@
+//
+//  ShowOrderCell.swift
+//  dawaay
+//
+//  Created by Yazan Alraddadi on 16/04/1443 AH.
+//
+
+import UIKit
+
+class ShowOrderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
