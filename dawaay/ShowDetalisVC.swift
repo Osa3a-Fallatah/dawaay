a96af98eed9e9d187175cefd9670b082c8ff8f58
yazzan-96 <yazanaraddadi@gmail.com>
2021-11-23 23:33:47 +0300

    added file and link cell , view delete table

---
+++ dawaay/ShowDetalisVC.swift
@@ -0,0 +1,38 @@
+//
+//  ShowDetalisVC.swift
+//  dawaay
+//
+//  Created by Yazan Alraddadi on 18/04/1443 AH.
+//
+
+import UIKit
+
+class ShowDetalisVC: UIViewController {
+    
+    @IBOutlet weak var Date: UILabel!
+    @IBOutlet weak var ShowImage: UIImageView!
+    @IBOutlet weak var IDNumber: UILabel!
+    @IBOutlet weak var Quantity: UILabel!
+    @IBOutlet weak var PhoneNumber: UILabel!
+    @IBOutlet weak var Location: UILabel!
+    @IBOutlet weak var MedicineName: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
