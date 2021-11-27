300602af0d9dc9634561e70d1f04e984a63d53b6
yazzan-96 <yazanaraddadi@gmail.com>
2021-11-27 14:09:46 +0300

    sign in and sign up

--- dawaay/LoginPatientVC.swift
+++ dawaay/LoginPatientVC.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class LoginPatientVC: UIViewController {
-    
+    var result = [Patient]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     @IBOutlet weak var userNamePatientLogin: UITextField!
     @IBOutlet weak var passwordPatientLogin: UITextField!
     
@@ -18,13 +19,36 @@ class LoginPatientVC: UIViewController {
         
     }
     @IBAction func patientLogin(_ sender: UIButton) {
-        if (userNamePatientLogin.text?.count != 0) && (passwordPatientLogin.text?.count != 0) {
-            let medicineVC = storyboard?.instantiateViewController(withIdentifier: "PatientID") as! MedicineVC
-            present(medicineVC, animated: true, completion: nil)
-        } else {
-            print("not allowed")
+        
+//        if (userNamePatientLogin.text?.count != 0) && (passwordPatientLogin.text?.count != 0) {
+//            let medicineVC = storyboard?.instantiateViewController(withIdentifier: "PatientID") as! MedicineVC
+//            present(medicineVC, animated: true, completion: nil)
+//        }
+//        else {
+//            print("not allowed")
+//        }
+        let request = Patient.fetchRequest()
+        var userName = userNamePatientLogin.text
+        var pass = passwordPatientLogin.text
+        
+        var filter = NSPredicate(format :
+                                    "userNameCONTAINS '\(userName!) ' AND password CONTAINS '\(pass!)'")
+        request.predicate = filter
+        
+        do {
+            try! result = context.fetch(request)
+            
+        }// append it to result array
+        if(result.count == 0) {
+            print ("the user not Found")
+            
+        }
+        else {
+            print ( "welcome")
+            print(result.count)
+            //go to proudect page}
+            
         }
     }
-    
-    
 }
+
--- dawaay/SignUpPatientVC.swift
+++ dawaay/SignUpPatientVC.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class SignUpPatientVC: UIViewController {
-
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     @IBOutlet weak var userNamePatient: UITextField!
     @IBOutlet weak var phoneNumberPatient: UITextField!
     @IBOutlet weak var passwordPatient: UITextField!
@@ -19,11 +20,26 @@ class SignUpPatientVC: UIViewController {
     }
     
     @IBAction func signUpPatient(_ sender: UIButton) {
-        if (userNamePatient.text?.count != 0) && (phoneNumberPatient.text?.count != 0) && (passwordPatient.text?.count != 0) {
-            let patientLoginVC = storyboard?.instantiateViewController(withIdentifier: "LoginPatientID") as! LoginPatientVC
-            present(patientLoginVC, animated: true, completion: nil)
-        } else {
-            print("not allowed")
+//        if (userNamePatient.text?.count != 0) && (phoneNumberPatient.text?.count != 0) && (passwordPatient.text?.count != 0) {
+//            let patientLoginVC = storyboard?.instantiateViewController(withIdentifier: "LoginPatientID") as! LoginPatientVC
+//            present(patientLoginVC, animated: true, completion: nil)
+//        }
+//        else {
+//            print("not allowed")
+//
+            
+            let newPatient = Patient (context: context)
+            newPatient.userName = userNamePatient.text
+            newPatient.password = passwordPatient.text
+//            newPatient.phoneNumber = phoneNumberPatient.text
+          
+            // save Context
+            do
+            {
+            try! context.save()
+            print ("saved !")
+            }
+            }
         }
-    }
-}
+    
+
