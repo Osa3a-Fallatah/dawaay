3428dbfdc71d10037d0ea55a8d4c971605b1eb19
yazzan-96 <91460505+yazzan-96@users.noreply.github.com>
2021-11-28 11:53:49 +0300

    Login singup (#5)
    * test
    
    * test
    
    * added file and link cell , view
    delete table
    
    * sign in and sign up
    
    * Sign in / log in

--- dawaay/LoginPatientVC.swift
+++ dawaay/LoginPatientVC.swift
@@ -6,25 +6,56 @@
 //
 
 import UIKit
+import CoreData
 
 class LoginPatientVC: UIViewController {
-    
+    var result = [Patient]()
+    var patientTable = PatientTable ()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     @IBOutlet weak var userNamePatientLogin: UITextField!
     @IBOutlet weak var passwordPatientLogin: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    }
+        }
     @IBAction func patientLogin(_ sender: UIButton) {
-        if (userNamePatientLogin.text?.count != 0) && (passwordPatientLogin.text?.count != 0) {
-            let medicineVC = storyboard?.instantiateViewController(withIdentifier: "PatientID") as! MedicineVC
+        
+//        if (userNamePatientLogin.text?.count != 0) && (passwordPatientLogin.text?.count != 0) {
+//            let medicineVC = storyboard?.instantiateViewController(withIdentifier: "PatientID") as! MedicineVC
+//            present(medicineVC, animated: true, completion: nil)
+//        }
+//        else {
+//            print("not allowed")
+//        }
+             
+        let request = Patient.fetchRequest() as NSFetchRequest<Patient>
+        let userName = userNamePatientLogin.text
+        let pass = passwordPatientLogin.text
+       
+        //filter we cam also use btewwn and others
+        //filter
+             var filter = NSPredicate(format: "userName == %@ AND password ==%@ ", userName!, pass! )
+              request.predicate = filter
+        
+        do {
+            self.result = try! self.context.fetch(request)
+          }// append it to result array
+        if(result.count == 0)
+        {
+            print ("the user not Found")
+            let medicineVC = storyboard?.instantiateViewController(withIdentifier: "LoginPatientID") as! LoginPatientVC
             present(medicineVC, animated: true, completion: nil)
-        } else {
-            print("not allowed")
+
+            
+        }
+        else {
+            print ( "welcome")
+            print(result.count)
+                let medicineVC = storyboard?.instantiateViewController(withIdentifier: "PatientID") as! MedicineVC
+                  present(medicineVC, animated: true, completion: nil)
         }
     }
-    
-    
+             
 }
+
---
+++ dawaay/ShowDetalisVC.swift
@@ -0,0 +1,21 @@
+//
+//  ShowDetalisVC.swift
+//  dawaay
+//
+//  Created by Yazan Alraddadi on 18/04/1443 AH.
+//
+
+import UIKit
+
+class ShowDetalisVC: UIViewController {
+    
+    @IBOutlet weak var Date: UILabel!
+    @IBOutlet weak var ShowImage: UIImageView!
+    @IBOutlet weak var IDNumber: UILabel!
+    @IBOutlet weak var Quantity: UILabel!
+    @IBOutlet weak var PhoneNumber: UILabel!
+    @IBOutlet weak var Location: UILabel!
+    @IBOutlet weak var MedicineName: UILabel!
+    
+    
+}
