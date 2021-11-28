50e52ee41d17b59f21117fd5dc4f4d143e3f9731
yazzan-96 <yazanaraddadi@gmail.com>
2021-11-28 11:53:21 +0300

    Sign in / log in

--- dawaay/LoginPatientVC.swift
+++ dawaay/LoginPatientVC.swift
@@ -6,9 +6,11 @@
 //
 
 import UIKit
+import CoreData
 
 class LoginPatientVC: UIViewController {
     var result = [Patient]()
+    var patientTable = PatientTable ()
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     @IBOutlet weak var userNamePatientLogin: UITextField!
     @IBOutlet weak var passwordPatientLogin: UITextField!
@@ -16,8 +18,7 @@ class LoginPatientVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    }
+        }
     @IBAction func patientLogin(_ sender: UIButton) {
         
 //        if (userNamePatientLogin.text?.count != 0) && (passwordPatientLogin.text?.count != 0) {
@@ -27,28 +28,34 @@ class LoginPatientVC: UIViewController {
 //        else {
 //            print("not allowed")
 //        }
-        let request = Patient.fetchRequest()
-        var userName = userNamePatientLogin.text
-        var pass = passwordPatientLogin.text
-        
-        var filter = NSPredicate(format :
-                                    "userNameCONTAINS '\(userName!) ' AND password CONTAINS '\(pass!)'")
-        request.predicate = filter
+             
+        let request = Patient.fetchRequest() as NSFetchRequest<Patient>
+        let userName = userNamePatientLogin.text
+        let pass = passwordPatientLogin.text
+       
+        //filter we cam also use btewwn and others
+        //filter
+             var filter = NSPredicate(format: "userName == %@ AND password ==%@ ", userName!, pass! )
+              request.predicate = filter
         
         do {
-            try! result = context.fetch(request)
-            
-        }// append it to result array
-        if(result.count == 0) {
+            self.result = try! self.context.fetch(request)
+          }// append it to result array
+        if(result.count == 0)
+        {
             print ("the user not Found")
+            let medicineVC = storyboard?.instantiateViewController(withIdentifier: "LoginPatientID") as! LoginPatientVC
+            present(medicineVC, animated: true, completion: nil)
+
             
         }
         else {
             print ( "welcome")
             print(result.count)
-            //go to proudect page}
-            
+                let medicineVC = storyboard?.instantiateViewController(withIdentifier: "PatientID") as! MedicineVC
+                  present(medicineVC, animated: true, completion: nil)
         }
     }
+             
 }
 
